f6426065342e5953f7b3a52a74dbdf852367d9e5
tlahuicole81 <el_caballero_jaguar@hotmail.com>
2025-12-03 02:07:12 -0600

    Add files via upload

---
+++ utilidades_y.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  LOCAL: 'http://localhost:3000/',
+  GAS: "https://script.google.com/macros/s/AKfycbwrsrmxcQ_ehq1i1_GtBnkXxpGhTg4bceaAqKvJPgX85Y7MN5pujEbwTz90gup_l2mh/exec", //prueba11',
+};
+
+// Definir la URL activa (puedes cambiar entre CONFIG.LOCAL y CONFIG.ESP32 según la prueba que realices)
+
+const URL_ACTIVA = CONFIG.GAS;
